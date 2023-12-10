80c4f0b7f091a1125283421a0ad99d2bf07e8ff8
Sean Davis <sean.dev.davis@gmail.com>
2023-12-10 08:47:54 -0600

    working on navbar

--- dranks-n-franks.ui/src/App.js
+++ dranks-n-franks.ui/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import Home from './Components/Home/Home';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          
+        </Routes>
+        <Home />
+      </BrowserRouter>
     </div>
   );
 }
